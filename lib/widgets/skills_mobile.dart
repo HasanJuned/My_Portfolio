ee19a7c2183fab38e0ed7a3ee40006790dec1295
Hassan <hasanahmad9812021@gmail.com>
2024-11-23 11:56:11 +0600

    updated for mobile

--- lib/widgets/skills_mobile.dart
+++ lib/widgets/skills_mobile.dart
@@ -10,29 +10,102 @@ class SkillsMobile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ConstrainedBox(
-      constraints: const BoxConstraints(
-        maxHeight: 550
-      ),
-      child: Column(
-        children: [
-          for(int i=0; i<skillItems.length; i++)
-            Container(
-              margin: const EdgeInsets.only(bottom: 5),
-              width: double.maxFinite,
-              decoration: BoxDecoration(
-                color: CustomColor.bgLight2,
-                borderRadius: BorderRadius.circular(5),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        ConstrainedBox(
+          constraints: const BoxConstraints(maxWidth: 700),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                'Work Experience',
+                style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: CustomColor.whitePrimary),
               ),
-              child: ListTile(
-                contentPadding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                leading: Image.asset(skillItems[i]['img'], width: 26,),
-                title: Text(skillItems[i]['title']),
+              const SizedBox(height: 8,),
+              Container(
+                width: 400,
+                decoration: BoxDecoration(
+                  color: CustomColor.scaffoldBg,
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                child: const ListTile(
+                  contentPadding:
+                  EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                  leading: Icon(
+                    Icons.work_history_outlined,
+                    size: 26,
+                  ),
+                  title: Text(
+                    'Teaching Assistant of Flutter\nOstad\n(Oct 2024 - Present)',
+                    style: TextStyle(color: CustomColor.whitePrimary),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 4,),
+              Container(
+                width: 400,
+                decoration: BoxDecoration(
+                  color: CustomColor.scaffoldBg,
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                child: const ListTile(
+                  contentPadding:
+                  EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                  leading: Icon(
+                    Icons.work_history_outlined,
+                    size: 26,
+                  ),
+                  title: Text(
+                    'Support Instructor of Flutter\nOstad\n(Aug,2023 - Oct 2024)',
+                    style: TextStyle(color: CustomColor.whitePrimary),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        const SizedBox(height: 12,),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              'Skills',
+              style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: CustomColor.whitePrimary),
+            ),
+            ConstrainedBox(
+              constraints: const BoxConstraints(
+                maxHeight: 550
+              ),
+              child: Column(
+                children: [
+                  for(int i=0; i<skillItems.length; i++)
+                    Container(
+                      margin: const EdgeInsets.only(bottom: 5),
+                      width: double.maxFinite,
+                      decoration: BoxDecoration(
+                        color: CustomColor.bgLight2,
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                      child: ListTile(
+                        contentPadding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                        leading: Image.asset(skillItems[i]['img'], width: 26,),
+                        title: Text(skillItems[i]['title']),
+                      ),
+                    ),
+
+                ],
               ),
             ),
-
-        ],
-      ),
+          ],
+        ),
+      ],
     );
   }
 }
