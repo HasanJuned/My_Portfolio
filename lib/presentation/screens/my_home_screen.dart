d578240c2dfc6c981d25f9320376fc9d3f235188
Hassan <hasanahmad9812021@gmail.com>
2024-11-23 11:47:25 +0600

    updated

--- lib/presentation/screens/my_home_screen.dart
+++ lib/presentation/screens/my_home_screen.dart
@@ -83,29 +83,6 @@ class _MyHomeScreenState extends State<MyHomeScreen> {
                   child: Column(
                     mainAxisSize: MainAxisSize.min,
                     children: [
-                       // const Padding(
-                       //   padding: EdgeInsets.only(left: 100.0),
-                       //   child: Row(
-                       //    //mainAxisAlignment: MainAxisAlignment.s,
-                       //    children: [
-                       //      Text(
-                       //        'Work Experience',
-                       //        style: TextStyle(
-                       //            fontSize: 24,
-                       //            fontWeight: FontWeight.bold,
-                       //            color: CustomColor.whitePrimary),
-                       //      ),
-                       //      SizedBox(width: 300,),
-                       //      Text(
-                       //        'Skills',
-                       //        style: TextStyle(
-                       //            fontSize: 24,
-                       //            fontWeight: FontWeight.bold,
-                       //            color: CustomColor.whitePrimary),
-                       //      ),
-                       //    ],
-                       //                         ),
-                       // ),
                       const SizedBox(height: 12,),
                       if (constraints.maxWidth >= kMedDesktopWidth)
                         const SkillsDesktop()
